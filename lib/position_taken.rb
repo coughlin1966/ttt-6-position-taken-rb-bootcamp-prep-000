a35ba7cde4b8d4c23864a8a981f0e38dbffade5e
Peter Coughlin <coughlin1966@gmail.com>
2018-02-09 17:24:08 +0000

    automatically backed up by learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,6 +1,6 @@
 def position_taken?(board, index)
-  board[index] != " "
-  #index != "" && index != " " && index != nil
-  #board[0] != nil
-
+  if board[index] != " "
+  elsif board[index] != ""
+  elsif board[] != nil
+end
 end
