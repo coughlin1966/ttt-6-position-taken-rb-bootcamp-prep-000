68073a14fd686d2705eda1074217f822e715d3a5
Peter Coughlin <coughlin1966@gmail.com>
2018-02-09 19:35:29 +0000

    automatically backed up by learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,12 +1,9 @@
 def position_taken?(board, index)
 
-   #if board[index] == " " || board[index] == "" || board[index] == nil
-   if 2 > 1
-     false
-
+  # if board[index] == " " || board[index] == "" || board[index] == nil
+  #   false
    #elsif board[index] == "X" || board[index] == "O"
-    elsif 5 < 3
-     true
-   end
-
+    # true
+   #end
+! (board[index] ==  " " || board[index] == "" || board[index] == nil) 
 end
